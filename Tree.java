332525373385807812363873a01533171cc6c681
beccinha <rebeccanascimento1@hotmail.com>
2016-04-20 16:28:28 -0300

    Update Tree.java
    falta o metodo excluir!

--- Tree.java
+++ Tree.java
@@ -5,14 +5,14 @@ public class Tree {
 	public Tree() {
 	}
 
-	public boolean add(int numero) {
+	public boolean add(int numero) { /* transformei em boolean so para saber se estava add direitinho*/
 		boolean retorno = false;
-		if (root == null) {
+		if (root == null) { /*so para o caso de não ter nd add na arvore*/
 			root = new NodeTree(numero);
 			retorno = true;
 		} else {
 			aux = root;
-			if (aux.getRight() == null && numero > aux.getInfo()) {
+			if (aux.getRight() == null && numero > aux.getInfo()) { 
 				aux.getRight().setInfo(numero);
 				retorno = true;
 			} else if (aux.getLeft() == null && numero < aux.getInfo()) {
@@ -71,12 +71,18 @@ public class Tree {
 					auxPai = aux;
 					aux = aux.getRight();
 				} else{
-					if(aux.getLeft() != null ){
+					if(aux.getLeft() == null ){
 						aux.setInfo(aux.getRight().getInfo());
 						retorno = true;
-					}else if(aux.getRight() != null){
+					}else if(aux.getRight() == null){
 						aux.setInfo(aux.getLeft().getInfo());
 						retorno = true;
+					} else if(aux.getLeft() != null && aux.getRight() != null){
+						aux = aux.getRight();
+						while(aux != null){
+							aux = aux.getLeft();
+						}
+
 					}
 				}
 			}
@@ -85,6 +91,52 @@ public class Tree {
 
 	public void searchElement(int numero) {
 		aux = root;
+		if (this.exist(info)) {
+			aux = root; // Aponta o no aux para a raiz
+			while (aux != null) { // Verifica se o aux nao e nulo
+				if (aux.getInfo() == info) { // Caso o valor do aux seja igual
+												// ao
+												// valor recebido
+					return aux;
+				} else {
+					if (info < aux.getInfo()) { // Verifica se o valor recebido
+												// e
+												// menor que o valor do aux
+						aux = aux.getLeft(); // Se for, entao aponta o aux para
+												// a
+												// esquerda do aux anterior
+					} else {
+						aux = aux.getRight();// Caso contrario, aponta o aux
+												// para a
+												// direita do aux anterior
+					}
+				}
+			}
+
+		}
+		return null;
+	}
+
+
+	public boolean exist(int info) { // Metodo para verificar se o no existe na
+										// arvore
+		aux = root; // Aponta o no aux para a raiz
+		while (aux != null) { // Verifica se o aux nao e nulo
+			if (aux.getInfo() == info) { // Caso o valor do aux seja igual ao
+											// valor recebido
+				return true;
+			} else {
+				if (info < aux.getInfo()) { // Verifica se o valor recebido e
+											// menor que o valor do aux
+					aux = aux.getLeft(); // Se for, entao aponta o aux para a
+											// esquerda do aux anterior
+				} else {
+					aux = aux.getRight();// Caso contrario, aponta o aux para a
+											// direita do aux anterior
+				}
+			}
+		}
+		return false;
 
 	}
 }
